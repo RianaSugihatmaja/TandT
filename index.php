75602babf74171388f323ec0ce62e1ed1b063a5d
unknown <anariana111992@gmail.com>
2019-02-10 20:06:10 +0700

    adding grid and dummies

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php echo file_get_contents("header.html"); ?>
-    <h1>Hallo</h1>
+<style>
+    .container{
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        margin-right: 0;
+        margin-left: 0;
+    }     
+</style>
+<div class="container">
+  <div class="row">
+    <div class="col col-lg-2">
+    </div>
+    <div class="col-6">
+    </div>
+    <div class="col col-lg-2">
+    </div>
+  </div>
+</div>
 <?php echo file_get_contents("footer.html"); ?>
--- newArrivals.php
+++ newArrivals.php
@@ -1,10 +1,44 @@
 <?php echo file_get_contents("header.html"); ?>
-  <div class="sidebarWrapper">
-    <ul class="sidebar">
-      <li class= "sidebar_list"><a href="notes.html">Note Books</a></li>
-      <li class= "sidebar_list"><a href="hijabs.html">Hijabs</a></li>
-      <li class= "sidebar_list"><a href="womensWear.html">Women's Wear</a></li>
-      <li class= "sidebar_list"><a href="accessories.html">Accessories</a></li>
+<style>
+.nav-link{
+  text-decoration: none;
+  list-style-type: none;
+  color: #000000;
+}
+.container{
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  margin-right: 0;
+  margin-left: 0;
+}
+</style>
+
+<div class="container">
+  <div class="row">
+  <div class="col col-lg-2">
+    <ul class="nav flex-sm-column">
+      <li class="nav-item"><a class="nav-link active" href="notes.html">Note Books</a></li>
+      <li class="nav-item"><a class="nav-link active" href="hijabs.html">Hijabs</a></li>
+      <li class="nav-item"><a class="nav-link active" href="womensWear.html">Women's Wear</a></li>
+      <li class="nav-item"><a class="nav-link active" href="accessories.html">Accessories</a></li>
     </ul>
-  </div>
+    </div>
+    <div class="col-10">
+      <div class="row">
+        <div class="col">
+        <a href="cart.php"><img src="img/book0.jpg" alt="Book Pic" height="200px" width="200px"></a>
+        <br><caption>Journal Basic</caption>  
+      </div>
+        <div class="col">
+        <a href="cart.php"><img src="img/book1.jpg" alt="Book Pic" height="200px" width="200px"></a>
+        <br><caption>Agenda Basic</caption>
+        </div>
+        <div class="col">
+        <a href="cart.php"><img src="img/book2.png" alt="Book Pic" height="200px" width="200px"></a>
+        <br><caption>Diary Basic</caption>
+        </div>
+      </div>
+    </div>
+</div>   
+</div>
   <?php echo file_get_contents("footer.html"); ?>
