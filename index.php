1bc2f8d2a8847cd8f57b8adb58e24cd1441d977a
unknown <anariana111992@gmail.com>
2019-02-10 16:12:41 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php echo file_get_contents("header.html"); ?>
+    <h1>Hallo</h1>
+<?php echo file_get_contents("footer.html"); ?>
---
+++ newArrivals.php
@@ -0,0 +1,10 @@
+<?php echo file_get_contents("header.html"); ?>
+  <div class="sidebarWrapper">
+    <ul class="sidebar">
+      <li class= "sidebar_list"><a href="notes.html">Note Books</a></li>
+      <li class= "sidebar_list"><a href="hijabs.html">Hijabs</a></li>
+      <li class= "sidebar_list"><a href="womensWear.html">Women's Wear</a></li>
+      <li class= "sidebar_list"><a href="accessories.html">Accessories</a></li>
+    </ul>
+  </div>
+  <?php echo file_get_contents("footer.html"); ?>
