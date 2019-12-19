451e82f5e2ebfc7f683d108b28a2fba23bc753e7
unknown <anariana111992@gmail.com>
2019-12-19 17:53:27 +0700

    continue article

--- blog.php
+++ blog.php
@@ -12,6 +12,13 @@
         <p>Journaling has been a part of my life since I was a little girl, not only that it helps me mentally, but it also helps me to stay creative and productive. I cannot stress it enough how writing down thoughts, feelings, ideas or even doodle on papers can help us to be on top of things. <br>
         As human we have limited ability to process and save information including but not limited to work, social or personal information. Internet and social media also play a big part in using up the space in our head. Overflowing information that we take in consciously or unconsciously can exhaust our mind and lead to several mental issues like anxiety, insecurity, panic attack, depression and more if it is not managed properly.<br>
         The awareness of mental health has significantly increased in the recent years especially in developed countries. Nowadays, there are many non-profit, private and governmental organizations providing therapy for people who need mental treatment. It is a big improvement that people have earned to be able to talk about their mental health more freely, since for the longest time talking about it is considered taboo and weak.
-        </p>
-</div>
+        </p><br>
+        <p>In addition to all those services, we can try to treat and help ourselves by journaling especially when those services are not accessible. I believe that all of us need to learn and practice to protect and treat ourselves from within. And one of the easiest, most accessible way is to journal. By writing down all the emotions that we are feeling, we can try to figure out a pattern of when those things come and bother us. From there we can try to make a game plan to caught ourselves and cut the chain of damage.
+        </p><br>
+    <img>
+    <br>
+        <p>There are many ways one can start journaling. You don’t have to choose which one is your genre, you can do all types of journaling that cater your need in the certain situation. These are some of journaling technics that I do:
+        </p><br>
+</div class="container">
+
 <?php echo file_get_contents("footer.html"); ?>
